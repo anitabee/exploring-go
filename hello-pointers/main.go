a4bb757fb4dd3724f1c703636920138abde24d77
Anita Bendelja <bendelja.anita@gmail.com>
2021-05-14 00:09:35 +0200

    Testing pointers

--- hello-pointers/main.go
+++ hello-pointers/main.go
@@ -13,14 +13,25 @@ func zeroVal(iVal int) {
 // address to the current value at that address. Assigning a value to a dereferenced pointer
 // changes the value at the referenced address.
 func zeroPtr(iPtr *int) {
-	*iPtr = 0 // set i through the pointer
+	*iPtr = 0 // set iPtr through the pointer
 }
 
 // Another way to get a pointer is to use the built-in new function.
 // new takes a type as an argument, allocates enough memory to fit a
 // value of that type and returns a pointer to it.
 func oneVal(iPtr *int) {
-	*iPtr = 1 // set i through the pointer
+	*iPtr = 1 // set iPtr through the pointer
+}
+
+func square(a *float64) {
+	*a = *a * *a
+}
+
+// http://www.golang-book.com/books/intro/8
+// Write a program that can swap two integers (x := 1; y := 2; swap(&x, &y) should give you x=2 and y=1)
+func swap(x *int, y *int) {
+	*x, *y = *y, *x
+
 }
 
 func main() {
@@ -33,10 +44,19 @@ func main() {
 	zeroPtr(&i)
 	fmt.Println("zeroPtr", i) // i is 0
 
+	fmt.Println("pointer:", &i) // print pointer value
+
 	iPtr := new(int)
 	oneVal(iPtr)
 	fmt.Println("oneVal", *iPtr) // iPtr is 1
 
-	fmt.Println("pointer:", &i) // print pointer value
+	a := 1.5
+	square(&a)
+	fmt.Println("square", a) // a is 2.25
+
+	x := 1
+	y := 2
+	swap(&x, &y)
+	fmt.Printf("swap x: %v, y: %v\n", x, y) // swap x and y values 
 
 }
